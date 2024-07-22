c5d722265a585a018ae21c10f9d437229668e8c8
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-21 23:00:31 -0700

    added js for city name

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+//state
+let currCity = "London";
+let units = "metric";
+
+//Selectors
+let city = document.querySelector(".weather-city");
+let datetime = document.querySelector(".weather-dataetime");
+
+//convert country code to name
+function convertCountryCode(country) {
+  let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+  return regionNames.of(country);
+}
+
 function getWeather() {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Turlock&appid=`${API_KEY}`"
+    `https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${API_KEY}&units=${units}`
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      city.innerHTML = `${data.name}, ${convertCountryCode(data.sys.country)}`;
+      // datetime.innerHTML =
+    });
 }
 
 document.body.addEventListener("load", getWeather());
