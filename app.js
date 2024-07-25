850d2947f5d5d92f527bbfce10460073e078dfef
Juan Pablo Davalos <davjuanpablo423@gmail.com>
2024-07-25 16:45:09 -0700

    added time of calculation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //state
-let currCity = "Turlock";
+let currCity = "London";
 let units = "imperial";
 
 //Selectors
@@ -10,6 +10,26 @@ let datetime = document.querySelector(".weather__datetime");
 function convertCountryCode(country) {
   let regionNames = new Intl.DisplayNames(["en"], { type: "region" });
   return regionNames.of(country);
+}
+
+//get local time at time zone
+function convertLocalTime(timestamp, timezone) {
+  const convertTimezone = timezone / 3600;
+  const date = new Date(timestamp * 1000);
+
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}${Math.abs(
+      convertTimezone
+    )}`,
+    hour12: true,
+  };
+  return date.toLocaleString("en-US", options);
 }
 
 async function getWeather() {
@@ -28,7 +48,7 @@ async function getWeather() {
 
   city.innerHTML = `${weatherData.name} , 
                     ${convertCountryCode(weatherData.sys.country)}`;
-  datetime.innerHTML = "hi";
+  datetime.innerHTML = convertLocalTime(weatherData.dt, weatherData.timezone);
 }
 
 document.body.addEventListener("load", getWeather());
